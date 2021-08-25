87070732ffa227557ed23988a0fc3670ec05f9f2
Federico Carbone <fcarbone@pingidentity.com>
2021-08-25 16:36:46 +0200

    Added project Change Background Color

---
+++ change-background-color/main.js
@@ -0,0 +1,25 @@
+const colors = ["bg-primary","bg-secondary","bg-success","bg-danger","bg-warning","bg-info"];
+let currentColorIndex = 1;
+
+window.onload = ()=> {
+    console.log ("Window onload");
+    document.getElementById ("chgColorBtn").addEventListener ("click", ()=> {
+        changeBackgroundColor ();
+    })
+}
+
+function changeBackgroundColor () {
+    let mainDivClassList = document.getElementById ("mainDiv").classList;
+    let newColorIndex = getNewColorIndex ();
+    mainDivClassList.remove (colors [currentColorIndex]);
+    mainDivClassList.add (colors [newColorIndex]);
+    currentColorIndex = newColorIndex;    
+}
+
+function getNewColorIndex () {
+    let colorIndex = currentColorIndex;
+    while (colorIndex == currentColorIndex) {
+        colorIndex = Math.floor (Math.random () * (colors.length - 1));
+    }
+    return colorIndex;
+}
