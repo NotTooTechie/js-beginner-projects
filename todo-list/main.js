f6fb09403e00d18683393a521e1efeb2fd06ef50
Federico Carbone <fcarbone@pingidentity.com>
2021-09-08 14:56:08 +0200

    Added todo list project

---
+++ todo-list/main.js
@@ -0,0 +1,42 @@
+let newTodoInput;
+let todos;
+
+window.onload = () => {
+    document.getElementById ("addTodoBtn").addEventListener ("click",()=> {
+        if (newTodoInput.value) {
+            addTodo ();
+        }
+        newTodoInput.focus ();
+    });
+
+    todos = document.getElementById ("todos");
+    newTodoInput = document.getElementById ("newTodoInput");
+}
+
+function addTodo () {
+    console.log (newTodoInput.value);
+    let todoCard = addNewElement ("div","card mb-2 shadow-sm",todos);
+    let todoBody = addNewElement ("div","card-body",todoCard);
+    let cardTitle = addNewElement ("h4","card-title",todoBody,newTodoInput.value);
+    let deleteButton = addNewElement ("button","btn btn-danger",todoBody, "Delete");
+    deleteButton.addEventListener ("click", ()=> {
+        todos.removeChild (todoCard);
+    })
+
+    newTodoInput.value = "";
+}
+
+
+function addNewElement (type,className, parent, innerText) {
+    element = document.createElement (type);
+    element.className = className;
+    if (innerText) {
+        element.innerText = innerText;    
+    }
+
+    if (parent) {
+        parent.appendChild (element);
+    }
+    
+    return element;
+}
