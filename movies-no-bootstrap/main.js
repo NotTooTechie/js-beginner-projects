a677feae783bd6c978d31e29d66167e0ea544219
Federico Carbone <fcarbone@pingidentity.com>
2021-09-15 16:47:49 +0200

    Added movies, with plain css

---
+++ movies-no-bootstrap/main.js
@@ -0,0 +1,71 @@
+const moviesUrl = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=";
+const imageBaseUrl = "https://image.tmdb.org/t/p/w300";
+let movieList;
+let currentPage = 1;
+let maxPages;
+
+window.onload = async () => {
+    movieList = document.getElementById("main");
+    let movies = await fetchMovies(currentPage);
+    maxPages = movies.total_pages;
+    displayMovies(movies);
+    if (currentPage < maxPages) {
+        currentPage++;
+        let movies = await fetchMovies(currentPage);
+        displayMovies(movies); 
+    }
+}
+
+window.addEventListener('scroll', async () => {
+    const {
+        scrollTop,
+        scrollHeight,
+        clientHeight
+    } = document.documentElement;
+
+    if (scrollTop + clientHeight >= scrollHeight - 5 && currentPage < maxPages) {
+        console.log("Bottom of the page. Scrolling");
+        currentPage++;
+        let movies = await fetchMovies(currentPage);
+        displayMovies(movies);
+    }
+}, {
+    passive: true
+});
+
+async function fetchMovies(page) {
+    let currentPage = page ? page : 1;
+    console.log("fetchMovies. Page " + page);
+    let movieRespnonse = await fetch(moviesUrl + currentPage);
+    let movies = await movieRespnonse.json();
+    return movies;
+}
+
+function displayMovies(movies) {
+    console.log(movies.results);
+    movies.results.forEach(movie => {
+        addMovie(movie);
+    });
+}
+
+function addMovie(movie) {
+    let card = addNewElement("div", "movie-card", movieList);
+    let image = addNewElement("img", "card-img-top", card)
+    image.src = imageBaseUrl + movie.backdrop_path;
+    let cardTitle = addNewElement("p", "movie-title", card, movie.original_title);
+}
+
+
+function addNewElement(type, className, parent, innerText) {
+    element = document.createElement(type);
+    element.className = className;
+    if (innerText) {
+        element.innerText = innerText;
+    }
+
+    if (parent) {
+        parent.appendChild(element);
+    }
+
+    return element;
+}
